1bf651998e04d8b3bc4c4e12e34f5585e39e14c8
fsanti <fsanti@sismotur.com>
2014-12-23 16:22:19 +0100

    Project rebuilt for Heroku

---
+++ README.md
@@ -0,0 +1,55 @@
+Web Sismotur (dev)
+============================
+
+THANKS TO: Express JS Static Boilerplate
+
+To launch: 
+Commit everything you need in git before launching
+
+$ heroku login
+
+Assume you are working in the development branch in git
+We always push to master branch of heroku
+
+$ git push heroku development:master
+
+Connect to URL: https://test-smt.herokuapp.com/
+or
+$ heroku open
+
+
+==================================================
+EXPRESS BOILERPLATE - THANKS TO MICHAEL LANCASTER
+==================================================
+
+http://www.bymichaellancaster.com/blog/deploying-nodejs-on-heroku-cloud/
+
+Express.js 4.0 Static Boilerplate is a simple Node.js Server using static files and directories for quick quality development and/or production.
+
+Features
+-----------
+- Static directory and files
+- Server watch for changes (Nodemon)
+- Grunt JS (Files Watch, CSS min, JS uglify)
+- no Jade
+
+Usage
+-----------
+On your terminal.
+
+Install the node packages.
+$ sudo npm install
+
+Run the server
+$ nodemon server.js
+
+
+To watch and run the Grunt.js task on any change to your main files "app.js, app.css"
+$ grunt watch
+
+Want to do manually?! no problem.
+$ grunt build
+
+Open your browser on the address 'localhost:xxxx' (xxxx=port chosen in server.js file)
+
+Done!
---
+++ public/js/sismotur.js
@@ -0,0 +1,96 @@
+/**
+ * @author Sismotur
+ */
+    		
+function loadScenes(){	
+	scene = new ScrollScene({triggerElement: "#trigger1"})
+					.setTween(TweenMax.to('#contactBanner',0.5,{css:{visibility: "hidden"}}))
+					.addTo(controller);	
+	
+	//scene.addIndicators();
+}
+
+function parallaxBanner(){
+	var controller = new ScrollMagic({globalSceneOptions: {triggerHook: "onLeave", duration: $('#smt-banner-par').height()}});
+	
+	var scene1 = new ScrollScene({triggerElement: "#parallaxTrigger"})
+			.setTween(TweenMax.fromTo("#smt-banner-par", 1, {backgrounPosition: "0 50px"}, {backgrounPosition: "0 -50px"}))
+			.addTo(controller);
+			
+	var scene2 = new ScrollScene({triggerElement: "#parallaxTrigger"})
+			.setTween(TweenMax.fromTo("#smt-banner-trans-par", 1, {autoAlpha: 0.2}, {autoAlpha: 1}))
+			.addTo(controller);
+			
+	var scene3 = new ScrollScene({triggerElement: "#parallaxTrigger"})
+			.setTween(TweenMax.fromTo("#smt-slogan-par", 1, {paddingTop: "25%"}, {paddingTop: "0%"}))
+			.addTo(controller);
+			
+	var scene4 = new ScrollScene({triggerElement: "#parallaxTrigger"})
+			.setTween(TweenMax.fromTo("#product", 1, {paddingTop: "50%"}, {paddingTop: "0%"}))
+			.addTo(controller);
+			
+	//scene1.addIndicators();
+	
+}
+
+function opaqueMenu(){
+	var controller = new ScrollMagic({globalSceneOptions: {triggerHook: 0.1, duration: 0}});
+	
+	var scene = new ScrollScene({triggerElement: "#menuTrigger"})
+			.setTween(TweenMax.to("#smt-menu", 0, {css:{backgroundColor: "rgba(0,36,168,1)"}}))
+			.addTo(controller);
+			
+	//scene.addIndicators();
+}
+
+function stopProducts(){
+	var controller = new ScrollMagic({globalSceneOptions: {triggerHook: 0.1, duration: 1000}});
+	
+	var scene = new ScrollScene({triggerElement: "#product"})
+				.setPin("#product")
+				.addTo(controller);
+				
+	//scene.addIndicators();
+}
+
+function showProductContents(){
+	var controller = new ScrollMagic({globalSceneOptions: {triggerHook: 50/$(window).height(), duration: 0}});
+		
+	var scene = new ScrollScene({triggerElement: "#product"})
+			.setTween(TweenMax.fromTo("#productsContent", 1.5, {autoAlpha: 0, x: -window.innerWidth}, {autoAlpha: 1, x: 0}))
+			.addTo(controller);
+			
+	//scene.addIndicators();
+}
+
+function stopTeam(){
+	var controller = new ScrollMagic({globalSceneOptions: {triggerHook: 50/$(window).height(), duration: 1000}});
+	
+	var scene = new ScrollScene({triggerElement: "#team"})
+				.setPin("#team")
+				.addTo(controller);
+				
+	//scene.addIndicators();
+}
+
+function parallaxTeam(){
+	var controller = new ScrollMagic({globalSceneOptions: {triggerHook: 50/$(window).height(), duration: $('#team').height()}});
+	
+	var scene1 = new ScrollScene({triggerElement: "#team"})
+			.setTween(TweenMax.fromTo("#team-tittle", 1, {paddingTop: "10%"}, {paddingTop: "0%"}))
+			.addTo(controller);
+			
+	var scene2 = new ScrollScene({triggerElement: "#team"})
+			.setTween(TweenMax.fromTo("#row1", 1, {paddingTop: "25%"}, {paddingTop: "0%"}))
+			.addTo(controller);
+			
+	var scene3 = new ScrollScene({triggerElement: "#team"})
+			.setTween(TweenMax.fromTo("#row2", 1, {paddingTop: "25%"}, {paddingTop: "0%"}))
+			.addTo(controller);
+			
+	var scene4 = new ScrollScene({triggerElement: "#team"})
+			.setTween(TweenMax.fromTo("#row3", 1, {paddingTop: "50%"}, {paddingTop: "0%"}))
+			.addTo(controller);
+			
+	//scene1.addIndicators();
+}
