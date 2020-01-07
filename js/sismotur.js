186565dbe06359c55023ac278659866ea9de496f
felipesanti <fsanti@sismotur.com>
2020-01-07 16:35:46 +0100

    Fix navigation bar in Firefox

--- js/sismotur.js
+++ js/sismotur.js
@@ -5,213 +5,256 @@
 function isMobile() {
   var device = navigator.userAgent;
 
-  return device.match(/Iphone/i) ||
-    device.match(/Ipod/i)||
-    device.match(/Android/i)||
-    device.match(/J2ME/i)||
-    device.match(/BlackBerry/i)||
-    device.match(/iPhone|iPad|iPod/i)||
-    device.match(/Opera Mini/i)||
-    device.match(/IEMobile/i)||
-    device.match(/Mobile/i)||
-    device.match(/Windows Phone/i)||
-    device.match(/windows mobile/i)||
-    device.match(/windows ce/i)||
-    device.match(/webOS/i)||
-    device.match(/palm/i)||
-    device.match(/bada/i)||
-    device.match(/series60/i)||
-    device.match(/nokia/i)||
-    device.match(/symbian/i)||
-    device.match(/HTC/i);
- }
+  return (
+    device.match(/Iphone/i) ||
+    device.match(/Ipod/i) ||
+    device.match(/Android/i) ||
+    device.match(/J2ME/i) ||
+    device.match(/BlackBerry/i) ||
+    device.match(/iPhone|iPad|iPod/i) ||
+    device.match(/Opera Mini/i) ||
+    device.match(/IEMobile/i) ||
+    device.match(/Mobile/i) ||
+    device.match(/Windows Phone/i) ||
+    device.match(/windows mobile/i) ||
+    device.match(/windows ce/i) ||
+    device.match(/webOS/i) ||
+    device.match(/palm/i) ||
+    device.match(/bada/i) ||
+    device.match(/series60/i) ||
+    device.match(/nokia/i) ||
+    device.match(/symbian/i) ||
+    device.match(/HTC/i)
+  );
+}
 
 function parallaxBanner() {
+  var controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      triggerHook: "onLeave",
+      duration: $("#parallaxTrigger").height()
+    }
+  });
 
-	var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onLeave", duration: $('#parallaxTrigger').height()}});
-
-	var scene2 = new ScrollMagic.Scene({triggerElement: "#parallaxTrigger"})
-			.setTween(TweenMax.fromTo("#fondo-azul", 1, {autoAlpha: 0.1}, {autoAlpha: 1}))
-			.addTo(controller);
-	//scene2.addIndicators();
+  var scene2 = new ScrollMagic.Scene({ triggerElement: "#parallaxTrigger" })
+    .setTween(
+      TweenMax.fromTo("#fondo-azul", 1, { autoAlpha: 0.1 }, { autoAlpha: 1 })
+    )
+    .addTo(controller);
+  //scene2.addIndicators();
 }
 
 function opaqueMenu() {
-	var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: 0.1, duration: 0}});
+  var controller = new ScrollMagic.Controller({
+    globalSceneOptions: { triggerHook: 0.1, duration: 0 }
+  });
 
-	var scene = new ScrollMagic.Scene({triggerElement: "#menuTrigger"})
-			.setTween(TweenMax.fromTo("#smt-menu", 0, {visibility: 'hidden'},{visibility: 'visible'}))
-			.addTo(controller);
+  var scene = new ScrollMagic.Scene({ triggerElement: "#menuTrigger" })
+    .setTween(
+      TweenMax.fromTo(
+        "#smt-menu",
+        0,
+        { visibility: "hidden" },
+        { visibility: "visible" }
+      )
+    )
+    .addTo(controller);
 
-	var scene2 = new ScrollMagic.Scene({triggerElement: "#menuTrigger"})
-			.setTween(TweenMax.fromTo("header", 0, {visibility: 'hidden'},{visibility: 'visible'}))
-			.addTo(controller);
+  var scene2 = new ScrollMagic.Scene({ triggerElement: "#menuTrigger" })
+    .setTween(
+      TweenMax.fromTo(
+        "header",
+        0,
+        { visibility: "hidden" },
+        { visibility: "visible" }
+      )
+    )
+    .addTo(controller);
 
-	//scene.addIndicators();
+  //scene.addIndicators();
 }
 
 function resizeIframe(obj) {
-    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+  obj.style.height = obj.contentWindow.document.body.scrollHeight + "px";
 }
 
 // Vídeo Montánchez, only in Spanish, but this can be called to make link dependent on language
-function videoYotube(){
+function videoYotube() {
   // get the cookie value or the navigator by default
-	var lang = getCookie('lang');
-	if (lang == "") { getDefaultNavigatorLanguage(); }
+  var lang = getCookie("lang");
+  if (lang == "") {
+    getDefaultNavigatorLanguage();
+  }
 
-	var video = "";
-	switch(lang){
-		case "es":
-			video = "https://www.youtube.com/watch?v=kOtfrKYdvNU";
-			break;
-		case "fr":
-			video = "https://www.youtube.com/watch?v=kOtfrKYdvNU";
-			break;
-		case "hr":
-			video = "https://www.youtube.com/watch?v=kOtfrKYdvNU";
-			break;
-		case "de":
-			video = "https://www.youtube.com/watch?v=kOtfrKYdvNU";
-			break;
-		default:
-			video = "https://www.youtube.com/watch?v=kOtfrKYdvNU";
-			break;
-	}
+  var video = "";
+  switch (lang) {
+    case "es":
+      video = "https://www.youtube.com/watch?v=kOtfrKYdvNU";
+      break;
+    case "fr":
+      video = "https://www.youtube.com/watch?v=kOtfrKYdvNU";
+      break;
+    case "hr":
+      video = "https://www.youtube.com/watch?v=kOtfrKYdvNU";
+      break;
+    case "de":
+      video = "https://www.youtube.com/watch?v=kOtfrKYdvNU";
+      break;
+    default:
+      video = "https://www.youtube.com/watch?v=kOtfrKYdvNU";
+      break;
+  }
 
-	window.open(video);
+  window.open(video);
 }
 
-function casoExito(){
+function casoExito() {
   // get the cookie value or the navigator by default
-	var lang = getCookie('lang');
-	if (lang == "") { getDefaultNavigatorLanguage(); }
+  var lang = getCookie("lang");
+  if (lang == "") {
+    getDefaultNavigatorLanguage();
+  }
 
-	var video = "";
-	switch(lang){
-		case "es":
-			video = "http://www.youtube.com/watch?v=A_DzRaJbtvM";
-			break;
-		case "fr":
-			video = "http://www.youtube.com/watch?v=QLQwePHyc08";
-			break;
-		/*case "hr":
+  var video = "";
+  switch (lang) {
+    case "es":
+      video = "http://www.youtube.com/watch?v=A_DzRaJbtvM";
+      break;
+    case "fr":
+      video = "http://www.youtube.com/watch?v=QLQwePHyc08";
+      break;
+    /*case "hr":
 			video = "http://www.youtube.com/watch?v=-PzUrNnvaFw";
 			break;*/
-		case "de":
-			video = "http://www.youtube.com/watch?v=Y7eKWSXKJcY";
-			break;
-		default:
-			video = "http://www.youtube.com/watch?v=RZTPCdGVrgw";
-			break;
-	}
+    case "de":
+      video = "http://www.youtube.com/watch?v=Y7eKWSXKJcY";
+      break;
+    default:
+      video = "http://www.youtube.com/watch?v=RZTPCdGVrgw";
+      break;
+  }
 
-	window.open(video);
+  window.open(video);
 }
 
-function inventrip(){
+function inventrip() {
   // get the cookie value or the navigator by default
-	var lang = getCookie('lang');
-	if (lang == "") { getDefaultNavigatorLanguage(); }
+  var lang = getCookie("lang");
+  if (lang == "") {
+    getDefaultNavigatorLanguage();
+  }
 
-	var url = "";
-	switch(lang) {
-		case "es":
-			url = "http://www.inventrip.com?lang=es";
-			break;
-		case "fr":
-			url = "http://www.inventrip.com?lang=fr";
-			break;
-		case "hr":
-			url = "http://www.inventrip.com?lang=hr";
-			break;
-		case "ca":
-			url = "http://www.inventrip.com?lang=ca";
-			break;
-		/*case "de":
+  var url = "";
+  switch (lang) {
+    case "es":
+      url = "http://www.inventrip.com?lang=es";
+      break;
+    case "fr":
+      url = "http://www.inventrip.com?lang=fr";
+      break;
+    case "hr":
+      url = "http://www.inventrip.com?lang=hr";
+      break;
+    case "ca":
+      url = "http://www.inventrip.com?lang=ca";
+      break;
+    /*case "de":
 			video = "http://www.inventrip.com?lang=de";
 			break;*/
-		default:
-			url = "http://www.inventrip.com?lang=en";
-			break;
-	}
+    default:
+      url = "http://www.inventrip.com?lang=en";
+      break;
+  }
 
-	window.open(url);
+  window.open(url);
 }
 
 // Opens a window with Inventrip Palencia in the right language
-function nfcPalencia(){
+function nfcPalencia() {
   // get the cookie value or the navigator by default
-	var lang = getCookie('lang');
-	if (lang == "") { getDefaultNavigatorLanguage(); }
+  var lang = getCookie("lang");
+  if (lang == "") {
+    getDefaultNavigatorLanguage();
+  }
 
-	var url = "";
-	switch(lang){
-		case "es":
-			url = "http://www.inventrip.com/diputaciondepalencia?lang=es";
-			break;
-		case "fr":
-			url = "http://www.inventrip.com/diputaciondepalencia?lang=fr";
-			break;
-		case "hr":
-			url = "http://www.inventrip.com/diputaciondepalencia?lang=hr";
-			break;
-		case "ca":
-			url = "http://www.inventrip.com/diputaciondepalencia?lang=ca";
-			break;
-		/*case "de":
+  var url = "";
+  switch (lang) {
+    case "es":
+      url = "http://www.inventrip.com/diputaciondepalencia?lang=es";
+      break;
+    case "fr":
+      url = "http://www.inventrip.com/diputaciondepalencia?lang=fr";
+      break;
+    case "hr":
+      url = "http://www.inventrip.com/diputaciondepalencia?lang=hr";
+      break;
+    case "ca":
+      url = "http://www.inventrip.com/diputaciondepalencia?lang=ca";
+      break;
+    /*case "de":
 			video = "http://www.inventrip.com/diputaciondepalencia?lang=de";
 			break;*/
-		default:
-			url = "http://www.inventrip.com/diputaciondepalencia?lang=en";
-			break;
-	}
+    default:
+      url = "http://www.inventrip.com/diputaciondepalencia?lang=en";
+      break;
+  }
 
-	window.open(url);
+  window.open(url);
 }
 
 /**
-* Will open the inventrip passport in a new window based upon the user language
-*/
+ * Will open the inventrip passport in a new window based upon the user language
+ */
 function doOpenPassportInventrip() {
   // get the cookie value or the navigator by default
-	var lang = getCookie('lang');
-	if (lang == "") { lang = getDefaultNavigatorLanguage(); }
-    console.log(lang);
+  var lang = getCookie("lang");
+  if (lang == "") {
+    lang = getDefaultNavigatorLanguage();
+  }
+  console.log(lang);
 
-	var url = "";
-	switch(lang){
-		case "es":
-			url = "https://www.flipsnack.com/inventrip/pasaporte_inventrip_es-fcpiwkkzy.html";
-			break;
-		case "pt":
-			url = "https://www.flipsnack.com/inventrip/passaporte_inventrip_pt.html";
-			break;
+  var url = "";
+  switch (lang) {
+    case "es":
+      url =
+        "https://www.flipsnack.com/inventrip/pasaporte_inventrip_es-fcpiwkkzy.html";
+      break;
+    case "pt":
+      url = "https://www.flipsnack.com/inventrip/passaporte_inventrip_pt.html";
+      break;
     case "fr":
-			url = "https://www.flipsnack.com/inventrip/passeport_inventrip_fr-fd1jlhhv0.html";
-			break;
+      url =
+        "https://www.flipsnack.com/inventrip/passeport_inventrip_fr-fd1jlhhv0.html";
+      break;
     case "ca":
       url = "https://www.flipsnack.com/sismotur/passaport_ca.html";
       break;
-		default: // english
+    default:
+      // english
       url = "https://www.flipsnack.com/sismotur/passport_en-fxk5enn1w.html";
-			break;
-	}
+      break;
+  }
 
-	window.open(url);
+  window.open(url);
 }
 
 /**
-* Returns the value of a cookie based on its input
-* @param input the cookie key (e.g. lang)
-* @return the value associated with the key
-*/
+ * Returns the value of a cookie based on its input
+ * @param input the cookie key (e.g. lang)
+ * @return the value associated with the key
+ */
 function getCookie(input) {
-  var cookies = document.cookie.split(';');
+  var cookies = document.cookie.split(";");
   // console.log('getCookie - cookies:' + cookies)
   for (var i = 0; i < cookies.length; i++) {
-    var name = cookies[i].split('=')[0].toLowerCase().trim();
-    var value = cookies[i].split('=')[1].toLowerCase().trim();
+    var name = cookies[i]
+      .split("=")[0]
+      .toLowerCase()
+      .trim();
+    var value = cookies[i]
+      .split("=")[1]
+      .toLowerCase()
+      .trim();
     // console.log('getCookie - name:' + name);
     // console.log('getCookie - value:' + value);
     if (name === input) {
@@ -222,11 +265,11 @@ function getCookie(input) {
   }
 
   return "";
-};
+}
 
 /**
-* Returns the default navigator language
-*/
+ * Returns the default navigator language
+ */
 function getDefaultNavigatorLanguage() {
-  return navigator.language.toLowerCase().split('-')[0]
+  return navigator.language.toLowerCase().split("-")[0];
 }
