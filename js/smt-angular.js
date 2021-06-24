26069991ed646c55b898b66348e0e86f3b460024
felipesanti <fsanti@MacBook-Pro.home>
2021-06-24 18:21:00 +0200

    Bump copyright 2021

--- js/smt-angular.js
+++ js/smt-angular.js
@@ -214,7 +214,7 @@ function loadLanguage($scope) {
             $scope.cntFrance2 = "Annecy";
             $scope.cntFrancePhone = "+33 (0)619.20.30.61";
 
-            $scope.copyright = "Copyright © 2017 SISMOTUR SL.";
+            $scope.copyright = "Copyright © 2021 SISMOTUR SL.";
 
             // *********************
             // NEWS ES
